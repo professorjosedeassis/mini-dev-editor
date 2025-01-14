2c04b5fd804fca6b7ac6c0ce51487306294b0363
José de Assis <josedeassisfilho@gmail.com>
2025-01-14 11:15:54 -0300

    aula 9

--- src/views/renderer.js
+++ src/views/renderer.js
@@ -1,7 +1,28 @@
+const linha = document.getElementById('linhas')
 const area = document.getElementById('txtArea')
 area.focus()
 
 //mudar a cor do texto
 api.setColor((event, color) => {
     area.style.color = color
+})
+
+//linhas
+function numerarLinhas() {
+    let linhaNumerada = ""
+    let arrayLinhas = area.value.split('\n')
+    for(let i = 0; i < arrayLinhas.length; i++) {
+        linhaNumerada += i + 1 + '<br>'
+        linha.innerHTML = linhaNumerada
+    }
+}
+
+numerarLinhas()
+
+area.addEventListener('input', () => {
+    numerarLinhas()
+})
+
+area.addEventListener('scroll', () => {
+    linha.scrollTop = area.scrollTop
 })
